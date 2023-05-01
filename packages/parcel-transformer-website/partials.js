97f44df362bd2c216bf61b06d490e2e5fb486335
Jirka-Mayer <jirkamayer97@gmail.com>
2023-05-01 11:48:50 +0200

    Added HTML partials logic

--- packages/parcel-transformer-website/partials.js
+++ packages/parcel-transformer-website/partials.js
@@ -1,35 +1,64 @@
-// https://github.com/rubenkaiser/parcel-plugin-html-partials
-
 const Transformer = require('@parcel/plugin').Transformer;
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
+async function resolveIncludeDirectives(filePath, originalCode) {
+  const regex = /@include\(["']([a-zA-Z0-9\/\-_.]*)["']\)/g;
+  const resolvedCode = [];
+  const dependencyPaths = [];
+  
+  let modified = false;
+  let resolvedUpTo = 0;
+
+  let match;
+  while ((match = regex.exec(originalCode)) !== null) {
+    resolvedCode.push(
+      originalCode.substring(resolvedUpTo, match.index)
+    );
+    resolvedUpTo = regex.lastIndex;
+
+    let includePath = path.join(path.dirname(filePath), match[1]);
+    dependencyPaths.push(includePath);
+
+    const includedContent = await fs.readFile(includePath, "utf-8");
+    const {
+      resolvedCode: includedResolvedContent,
+      dependencyPaths: subDependencyPaths
+    } = await resolveIncludeDirectives(includePath, includedContent);
+    resolvedCode.push(includedResolvedContent);
+    for (let path of subDependencyPaths)
+      dependencyPaths.push(path);
+
+    modified = true;
+  }
+
+  if (modified) {
+    resolvedCode.push(
+      originalCode.substring(resolvedUpTo, originalCode.length)
+    );
+  }
+
+  return {
+    modified: modified,
+    resolvedCode: modified ? resolvedCode.join("") : originalCode,
+    dependencyPaths
+  };
+}
+
 module.exports = new Transformer({
-  async transform(props) {
-    const { asset, options } = props;
+  async transform({ asset, options, logger }) {
+    const originalCode = await asset.getCode();
 
-    let code = await asset.getCode();
-    const originalCode = code;
+    const {
+      modified,
+      resolvedCode,
+      dependencyPaths
+    } = await resolveIncludeDirectives(asset.filePath, originalCode);
 
-    const partialsRegex = /@partial\(["']([a-zA-Z0-9\/\-_.]*)["']\)/g;
-    let match;
-
-    const includes = [];
-
-    while ((match = partialsRegex.exec(code)) !== null) {
-      let partial = path.resolve(options.projectRoot, match[1]);
-      includes.push(partial);
-      if (fs.existsSync(partial)) { // eslint-disable-line no-sync
-        const partialContent = fs.readFileSync(partial, { encoding: 'utf-8' }); // eslint-disable-line no-sync
-        const partialRegex = new RegExp(match[0].replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1'), 'gi');
-        code = code.replace(partialRegex, partialContent);
-      }
-    }
-
-    if (code !== originalCode) {
-      asset.setCode(code);
-      for (let include of includes) {
-        asset.invalidateOnFileChange(include);
+    if (modified) {
+      asset.setCode(resolvedCode);
+      for (let path of dependencyPaths) {
+        asset.invalidateOnFileChange(path);
       }
     }
 
