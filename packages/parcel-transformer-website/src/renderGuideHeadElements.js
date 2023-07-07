df31f1e7415d50640d85dda681aebc72b423c49a
Jirka-Mayer <jirkamayer97@gmail.com>
2023-07-07 10:25:32 +0200

    Favicon and more meta tags

--- packages/parcel-transformer-website/src/renderGuideHeadElements.js
+++ packages/parcel-transformer-website/src/renderGuideHeadElements.js
@@ -1,5 +1,10 @@
+const tags = require("../../../src/tags.js");
+
 function renderGuideHeadElements(meta) {
   const parts = [];
+
+  // comment
+  parts.push(`<!-- guide head elements -->`);
 
   // don't index non-published articles
   if (!meta.datePublished) {
@@ -8,6 +13,23 @@ function renderGuideHeadElements(meta) {
 
   // the atricle canonical URL
   parts.push(`<link rel="canonical" href="${meta.parcelAssetPath}">`);
+
+  // keywords
+  const tagString = meta.tags.map(tagId => {
+    const tag = tags.filter(t => t.id === tagId)[0];
+    if (!tag || !tag.title) return undefined;
+    return tag.title;
+  })
+    .filter(t => t !== undefined)
+    .join(", ")
+    .replace("\"", "&quot;");
+  parts.push(`<meta name="keywords" content="${tagString}">`);
+
+  // author
+  if (meta.author) {
+    const authorString = meta.author.replace("\"", "&quot;");
+    parts.push(`<meta name="author" content="${authorString}">`);
+  }
   
   return parts.join("\n");
 }
